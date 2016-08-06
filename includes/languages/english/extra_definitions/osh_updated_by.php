d5521f78a172da4812c59cabbe6029aa116f0c71
lat9 <lat9@vinosdefrutastropicales.com>
2016-08-06 13:04:16 -0400

    Initial commit
    ... using the osh_updated_by v1.3.1 files as the basis.

---
+++ includes/languages/english/extra_definitions/osh_updated_by.php
@@ -0,0 +1,18 @@
+<?php
+// -----
+// Part of the "Orders Status - Updated By" plugin by lat9.  This is the store-side version of the language file.
+// Copyright (C) 2016, Vinos de Frutas Tropicales.
+//
+define('OSH_EMAIL_SEPARATOR', '------------------------------------------------------');
+define('OSH_EMAIL_TEXT_SUBJECT', 'Order Update');
+define('OSH_EMAIL_TEXT_ORDER_NUMBER', 'Order Number:');
+define('OSH_EMAIL_TEXT_INVOICE_URL', 'Detailed Invoice:');
+define('OSH_EMAIL_TEXT_DATE_ORDERED', 'Date Ordered:');
+define('OSH_EMAIL_TEXT_COMMENTS_UPDATE', '<em>The comments for your order are: </em>' . "\n\n");
+define('OSH_EMAIL_TEXT_STATUS_UPDATED', 'Your order\'s status has been updated:' . "\n");
+define('OSH_EMAIL_TEXT_STATUS_NO_CHANGE', 'Your order\'s status has not changed:' . "\n");
+define('OSH_EMAIL_TEXT_STATUS_LABEL', '<strong>Current status: </strong> %s' . "\n\n");
+define('OSH_EMAIL_TEXT_STATUS_CHANGE', '<strong>Old status:</strong> %1$s, <strong>New status:</strong> %2$s' . "\n\n");
+define('OSH_EMAIL_TEXT_STATUS_PLEASE_REPLY', 'Please reply to this email if you have any questions.' . "\n");
+
+define('SEND_EXTRA_ORDERS_STATUS_ADMIN_EMAILS_TO_SUBJECT','[ORDERS STATUS]');
